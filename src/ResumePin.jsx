c1cc4c335c923d6cc0cd62faef92c34c4e29a7c9
Digital <bogdantimes@gmail.com>
2024-10-26 20:18:42 +0100

    1.0.3

--- src/ResumePin.jsx
+++ src/ResumePin.jsx
@@ -3,7 +3,7 @@ import { Notes, PushPinOutlined } from '@mui/icons-material'
 
 const ResumePin = ({ fontSize = 'small' }) => (
     <>
-        <Notes sx={{ position: 'absolute', top: '15px' }} fontSize={fontSize} />
+        <Notes sx={{ position: 'absolute', top: '13px' }} fontSize={fontSize} />
         <PushPinOutlined fontSize={fontSize} />
     </>
 )
